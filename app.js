3dd9373b991c0cb30ba95723f9909bd5070742c8
Roshen Nair <roshen620@gmail.com>
2021-07-23 01:00:25 +0800

    Implement signup and login API endpoints

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const redis = require('redis');
 const connectRedis = require('connect-redis');
+const bcrypt = require('bcrypt');
+require('express-async-errors');
 
 // Constants
 const TWO_HOURS = 1000 * 60 * 60 * 2;
@@ -13,9 +15,11 @@ const {
 	NODE_ENV = 'development',
 	SESSION_NAME = 'sid',
 	SESSION_LIFETIME = TWO_HOURS,
-	SESSION_SECRET = 'd47e3ac2-1fc5-4d99-9c55-de055d38e302'
+	SESSION_SECRET = 'd47e3ac2-1fc5-4d99-9c55-de055d38e302',
+	SALT_ROUNDS = 10
 } = process.env;
 const IN_PROD = NODE_ENV === 'production';
+const users = []; // TODO: Replace with DB
 
 // Initializations
 const app = express();
@@ -40,11 +44,42 @@ app.use(session({
 }));
 
 // Routes
-app.post('/sign-up', (req, res) => {
-	console.log(req.body);
+app.post('/signup', async (req, res) => {
+	const { name, email, username, password } = req.body;
+
+
+	if (name && email && username && password) {
+		// Ensure user doesn't already exist
+		if (users.some(user => user.email === email)) {
+			return res.status(409).json({ error: 'User already exists' });
+		}
+
+		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+		const newUser = { id: users.length + 1, name, email, username, hashedPassword };
+		users.push(newUser);
+		req.session.userId = newUser.id;
+		res.status(200).json({ name, email, username });
+	} else {
+		res.status(400).json({ error: 'Missing sign up fields' });
+	}
 });
 
-app.post('/login', (req, res) => { });
+app.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+
+	if (username && password) {
+		const matchingUser = users.find(user => (user.username === username) || (user.email === username));
+		if (!matchingUser) return res.status(404).json({ error: 'User not found' });
+
+		const passwordsMatch = await bcrypt.compare(password, matchingUser.password);
+		if (!passwordsMatch) return res.status(401).json({ error: 'Incorrect password' });
+
+		req.session.userId = matchingUser.id;
+		res.status(200).json({ name: matchingUser.name, email: matchingUser.email, username: matchingUser.username });
+	} else {
+		res.status(400).json({ error: 'Missing login fields' });
+	}
+});
 
 // Start server
 app.listen(PORT, () => console.log(`Homina server running on port ${PORT}...`));
