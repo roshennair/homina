592d7f7aa619c4ced4c3075f70773cada71223c7
Roshen Nair <roshen620@gmail.com>
2021-07-25 23:24:59 +0800

    Implement logout API endpoint

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const TWO_HOURS = 1000 * 60 * 60 * 2;
 const {
 	PORT = 5000,
 	NODE_ENV = 'development',
+	REDIS_URL = 'redis://localhost:6379/',
 	SESSION_NAME = 'sid',
 	SESSION_LIFETIME = TWO_HOURS,
 	SESSION_SECRET = 'd47e3ac2-1fc5-4d99-9c55-de055d38e302',
@@ -24,7 +25,7 @@ const users = []; // TODO: Replace with DB
 // Initializations
 const app = express();
 const RedisStore = connectRedis(session);
-const redisClient = redis.createClient();
+const redisClient = redis.createClient(REDIS_URL);
 
 // Middleware
 app.use(morgan('dev'));
@@ -47,20 +48,22 @@ app.use(session({
 app.post('/signup', async (req, res) => {
 	const { name, email, username, password } = req.body;
 
-
 	if (name && email && username && password) {
 		// Ensure user doesn't already exist
-		if (users.some(user => user.email === email)) {
-			return res.status(409).json({ error: 'User already exists' });
+		if (users.some(user => user.username === username)) {
+			return res.status(409).json({ message: 'Username already exists.', field: 'username' });
+		} else if (users.some(user => user.email === email)) {
+			return res.status(409).json({ message: 'Email already exists.', field: 'email' });
 		}
 
 		const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-		const newUser = { id: users.length + 1, name, email, username, hashedPassword };
+		const newUser = { id: users.length + 1, name, email, username, password: hashedPassword };
 		users.push(newUser);
 		req.session.userId = newUser.id;
+		console.log(users);
 		res.status(200).json({ name, email, username });
 	} else {
-		res.status(400).json({ error: 'Missing sign up fields' });
+		res.status(400).json({ message: 'Missing sign up fields.', field: '' });
 	}
 });
 
@@ -69,16 +72,24 @@ app.post('/login', async (req, res) => {
 
 	if (username && password) {
 		const matchingUser = users.find(user => (user.username === username) || (user.email === username));
-		if (!matchingUser) return res.status(404).json({ error: 'User not found' });
+		if (!matchingUser) return res.status(404).json({ message: 'Username not found.', field: 'username' });
 
 		const passwordsMatch = await bcrypt.compare(password, matchingUser.password);
-		if (!passwordsMatch) return res.status(401).json({ error: 'Incorrect password' });
+		if (!passwordsMatch) return res.status(401).json({ message: 'Incorrect password.', field: 'password' });
 
 		req.session.userId = matchingUser.id;
 		res.status(200).json({ name: matchingUser.name, email: matchingUser.email, username: matchingUser.username });
 	} else {
-		res.status(400).json({ error: 'Missing login fields' });
+		res.status(400).json({ message: 'Missing login fields.', field: '' });
 	}
+});
+
+app.post('/logout', async (req, res) => {
+	req.session.destroy(err => {
+		if (err) return res.status(500).json({ message: 'Error logging out.', field: '' });
+		res.clearCookie(SESSION_NAME);
+		res.sendStatus(200);
+	});
 });
 
 // Start server
