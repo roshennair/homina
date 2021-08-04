d07ef344dc7b44972e7800db55b669c0772fc9c5
Roshen Nair <roshen620@gmail.com>
2021-08-04 15:07:58 +0800

    Rename server script from app.js to server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+// Imports
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const morgan = require('morgan');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+require('express-async-errors');
+const authRoutes = require('./routes/auth');
+
+// Constants
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+const {
+	PORT = 5000,
+	NODE_ENV = 'development',
+	REDIS_URL = 'redis://localhost:6379/',
+	SESSION_NAME = 'sid',
+	SESSION_LIFETIME = TWO_HOURS,
+	SESSION_SECRET = 'd47e3ac2-1fc5-4d99-9c55-de055d38e302',
+} = process.env;
+const IN_PROD = NODE_ENV === 'production';
+
+// Initializations
+const app = express();
+app.set("trust proxy", 1);
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient(REDIS_URL);
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(session({
+	name: SESSION_NAME,
+	resave: false,
+	saveUninitialized: false,
+	secret: SESSION_SECRET,
+	cookie: {
+		maxAge: SESSION_LIFETIME,
+		sameSite: true,
+		secure: IN_PROD
+	},
+	store: new RedisStore({ client: redisClient })
+}));
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Authentication routes
+app.use('/auth', authRoutes);
+
+// Serve client
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Homina server running on port ${PORT}...`));
